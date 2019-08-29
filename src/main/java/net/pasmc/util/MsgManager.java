3470b987a6c6699140450324b612e45edc459cd7
woaijpl00 <i@pasmc.net>
2019-08-29 23:33:55 +0800

    Manager added.

---
+++ src/main/java/net/pasmc/util/MsgManager.java
@@ -0,0 +1,54 @@
+package net.pasmc.util;
+
+import com.sobte.cqp.jcq.entity.IMsg;
+
+public class MsgManager {
+
+    /**
+     * 私聊消息 (Type=21)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subType 子类型，11/来自好友 1/来自在线状态 2/来自群 3/来自讨论组
+     * @param msgId   消息ID
+     * @param fromQQ  来源QQ
+     * @param msg     消息内容
+     * @param font    字体
+     * @return 返回值*不能*直接返回文本 如果要回复消息，请调用api发送<br>
+     * 这里 返回  {@link IMsg#MSG_INTERCEPT MSG_INTERCEPT} - 截断本条消息，不再继续处理<br>
+     * 注意：应用优先级设置为"最高"(10000)时，不得使用本返回值<br>
+     * 如果不回复消息，交由之后的应用/过滤器处理，这里 返回  {@link IMsg#MSG_IGNORE MSG_IGNORE} - 忽略本条消息
+     */
+
+    public static void onPrivateMsgReceived(int subType, int msgId, long fromQQ, String msg, int font) {
+    }
+
+    /**
+     * 群消息 (Type=2)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subType       子类型，目前固定为1
+     * @param msgId         消息ID
+     * @param fromGroup     来源群号
+     * @param fromQQ        来源QQ号
+     * @param fromAnonymous 来源匿名者
+     * @param msg           消息内容
+     * @param font          字体
+     */
+
+    public static void onGroupMsgReceived(int subType, int msgId, long fromGroup, long fromQQ, String fromAnonymous, String msg, int font) {
+    }
+
+    /**
+     * 讨论组消息 (Type=4)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subtype     子类型，目前固定为1
+     * @param msgId       消息ID
+     * @param fromDiscuss 来源讨论组
+     * @param fromQQ      来源QQ号
+     * @param msg         消息内容
+     * @param font        字体
+     */
+    public static void onDiscussMsgReceived(int subtype, int msgId, long fromDiscuss, long fromQQ, String msg, int font) {
+    }
+}
